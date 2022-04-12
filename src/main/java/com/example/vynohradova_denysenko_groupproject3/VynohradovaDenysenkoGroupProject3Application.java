53105ebdf5cd16cbb8d10c2f9f453e3279eaf2c3
alisondraV <alice.7414122013@gmail.com>
2022-04-12 16:19:39 -0400

    Initial commit

---
+++ src/main/java/com/example/vynohradova_denysenko_groupproject3/VynohradovaDenysenkoGroupProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.vynohradova_denysenko_groupproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VynohradovaDenysenkoGroupProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VynohradovaDenysenkoGroupProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vynohradova_denysenko_groupproject3/VynohradovaDenysenkoGroupProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vynohradova_denysenko_groupproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VynohradovaDenysenkoGroupProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
