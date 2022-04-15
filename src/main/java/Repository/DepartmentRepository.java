36a53248af7557fd54e1d86ee3309aa14ecf5fe8
alisondraV <alice.7414122013@gmail.com>
2022-04-15 12:51:17 -0400

    Set up a template for the future tasks and complete three more of them

--- src/main/java/Repository/DepartmentRepository.java
+++ src/main/java/Repository/DepartmentRepository.java
@@ -3,5 +3,9 @@ package Repository;
 import Model.*;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends CrudRepository<DepartmentsEntity, Integer> {
+    DepartmentsEntity findByDepartmentName(String name);
+    List<DepartmentsEntity> findByLocationId(Integer id);
 }
